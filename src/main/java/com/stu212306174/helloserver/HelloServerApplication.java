a117e42e06a423a8aee5bacff6442985c8083531
Walnutii <1907881197@qq.com>
2026-03-05 21:24:24 +0800

    hello-server

---
+++ src/main/java/com/stu212306174/helloserver/HelloServerApplication.java
@@ -0,0 +1,13 @@
+package com.stu212306174.helloserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServerApplication.class, args);
+    }
+
+}
